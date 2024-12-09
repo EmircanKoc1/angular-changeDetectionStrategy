e5ced7536d7b20189461c2ce420515ca823ede7f
Emircan <emircandv787@gmail.com>
2024-12-09 23:41:29 +0300

    The components have been defined.

---
+++ src/app/components/defaultStrategy/child3/child3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-child3',
+  standalone: true,
+  imports: [],
+  templateUrl: './child3.component.html',
+  styleUrl: './child3.component.scss'
+})
+export class Child3Component {
+
+}
